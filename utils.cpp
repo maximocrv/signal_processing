03847dd6452d6562cce4bc32c8f8fe901fed5bb2
Maximo Cravero <maximo@geronimo.ai>
2020-11-22 21:43:50 +0100

    overview of reading function requirements

---
+++ utils.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Maximo Cravero on 22/11/2020.
+//
+
+// TODO: Include exceptions, function overloading.
+// TODO: include exception catching!!
+
+// Function declarations
+void ReadFile(std::string fileName){};
+
+// Read data from file
+
+
+// Read data from stdin (command line)
+
+
+// Read file from user input
+
+
+// Function definitions
