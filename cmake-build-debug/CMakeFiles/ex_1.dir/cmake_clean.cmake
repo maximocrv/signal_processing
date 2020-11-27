803874cf5db91b47d186a83413bc1531d72e31ff
Maximo Cravero <maximo@geronimo.ai>
2020-11-27 11:34:50 +0100

    remove audiofile as cloned repo

---
+++ cmake-build-debug/CMakeFiles/ex_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex_1.dir/Output.cpp.o"
+  "ex_1"
+  "ex_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
