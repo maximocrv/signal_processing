a7c7ca1fa62f0dee517e5186068440eefd7aaaf8
Maximo Cravero <maximo@geronimo.ai>
2020-11-27 08:22:06 +0100

    adjusted cmakelists

--- utils.cpp
+++ utils.cpp
@@ -5,17 +5,17 @@
 // TODO: Include exceptions, function overloading.
 // TODO: include exception catching!!
 
-#include "Audiofile/Audiofile.h"
+#include <iostream>
+
+#include "AudioFile/AudioFile.h"
+
+int main(){
+    std::cout << "hello" << std::endl;
+}
+
 // Function declarations
-void ReadFile(std::string fileName);
+//void ReadFile(std::string fileName);fileName
 
-// Read data from file
-
-
-// Read data from stdin (command line)
-
-
-// Read file from user input
 
 
 // Function definitions
